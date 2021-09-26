c60afed8470e8f27250d87ab9953a263e897a6c8
Эржигит Имамов <erjigit17@gmail.com>
2021-09-26 17:11:35 +0600

    add session

--- session/server.js
+++ session/server.js
@@ -43,3 +43,30 @@ const types = {
   undefined: () => 'not found',
 };
 
+http.createServer(async (req, res) => {
+  const client = await Client.getInstance(req, res);
+  const { method, url, headers } = req.headers;
+  console.log(`${method} ${url} ${headers.cookie}`);
+  const handler = routing[url];
+  res.on('finish', () => {
+    if (client.session) client.session.save();
+  });
+  if (!handler) {
+    res.statusCode = 404;
+    res.end('Not found 404');
+    return;
+  }
+  handler(client).then(data => {
+    const type = typeof data;
+    const serializer = types[type];
+    const result =serializer(data);
+    client.sendCookie();
+    res.end(result);
+  }, err => {
+    res.statusCode = 500;
+    res.end('Internal Server Error 500: ' + err.message);
+    console.log(err);
+  });
+}).listen(8000)
+
+
